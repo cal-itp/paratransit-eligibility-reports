4f18e02cfe153a20766dacbff36eec2b37996e27
hunterowens <owens.hunter@gmail.com>
2020-12-09 18:06:28 -0800

    download the gsheet

---
+++ download-official-gsheet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ wget -O "intermediate/offical_list_from_gsheet.csv" "https://docs.google.com/spreadsheets/d/1lMg8d93MvVyO5OG9YTDfYxiYmIzgfji0ytpymMXzT8g/export?format=csv&gid=1448093633"
